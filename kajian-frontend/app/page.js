410da3ee3be882122a3c115802893cb8a04801da
mhmdramadhan <mr.mhmd.ramadhan@gmail.com>
2025-04-09 05:44:50 +0700

    Initialize Next.js frontend with Tailwind CSS, ESLint, and basic layout

---
+++ kajian-frontend/app/page.js
@@ -0,0 +1,8 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return <h1 className="text-3xl font-bold underline text-red-500">
+    Hello world!
+  </h1>
+}
