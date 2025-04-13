d8e4da69d45788de415a659d90eeb28171197b5c
mhmdramadhan <mr.mhmd.ramadhan@gmail.com>
2025-04-13 20:41:45 +0700

    Refactor Dashboard components to fetch data server-side; pass data as props to DashboardAdmin and DashboardUstadz for improved performance and session handling

--- kajian-frontend/app/admin/(admin-page)/dashboard/page.js
+++ kajian-frontend/app/admin/(admin-page)/dashboard/page.js
@@ -10,15 +10,56 @@ export default async function DashboardPage() {
     const session = await getServerSession(authOptions);
     if (!session) redirect("/admin/login");
 
-    return (
-        <div className="p-4">
-            {session.user.role === 'admin' && (
-                <DashboardAdmin />
-            )}
+    console.log(session.user);
+    
+    if (session.user.role == 'admin') {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE}/dashboard/admin-overview`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${session.user.token}`,
+            },
+            cache: "no-store",
+        });
 
-            {session.user.role === 'ustadz' && (
-                <DashboardUstadz />
-            )}
-        </div>
-    );
+        const data = await res.json();
+
+        console.log(data);
+        
+
+        return (
+            <div className="p-4">
+                <DashboardAdmin adminData={data} />
+
+            </div>
+        );
+    }
+
+    if (session.user.role == 'ustadz') {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE}/dashboard/ustadz-overview/${session.user.ustadzId}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${session.user.token}`,
+            },
+            cache: "no-store",
+        });
+
+        const data = await res.json();
+
+        return (
+            <div className="p-4">
+                <DashboardUstadz ustadzData={data} />
+            </div>
+        );
+    }
+
+
+
+
+
+
+
+
+
 }
--- kajian-frontend/components/DashboardUstadz.jsx
+++ kajian-frontend/components/DashboardUstadz.jsx
@@ -1,8 +1,7 @@
 'use client';
 
-import { getSession, signOut } from 'next-auth/react';
+import { signOut } from 'next-auth/react';
 import { toast } from 'sonner';
-import api from '@/utils/api';
 import {
     Chart as ChartJS,
     CategoryScale,
@@ -13,44 +12,24 @@ import {
     Legend,
 } from 'chart.js';
 import { Bar } from 'react-chartjs-2';
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
-export default function DashboardUstadz() {
-    const [ustadzData, getUstadzData] = useState(null);
-
-    // ambil data
-    useEffect(() => {
-        const fetchData = async () => {
-            const session = await getSession();
-            if (!session) return;
-
-            try {
-                const res = await api.get(`/dashboard/ustadz-overview/${session.user.ustadzId}`, {
-                    headers: { Authorization: `Bearer ${session.user.token}` }
-                });
-                getUstadzData(res.data);
-            } catch (err) {
-                console.error('Gagal mengambil data dashboard:', err.response?.data.message);
-
-                console.log('Gagal mengambil data dashboard:', err?.response?.data?.message);
-
-                if (err?.response?.data?.message === 'Token tidak valid') {
-                    toast.error('Sesi login kamu sudah habis. Silakan login ulang.');
-                    setTimeout(() => {
-                        signOut({ callbackUrl: '/admin/login' });
-                    }, 2000);
-                }
-            }
-        };
-
-        fetchData();
-    }, [])
-
+export default function DashboardUstadz({ ustadzData }) {
     if (!ustadzData) {
-        return <div>Loading...</div>;
+        return <div className="text-center text-gray-500">Memuat data dashboard...</div>;
     }
+
+    useEffect(() => {
+        // jika token habis
+        if (ustadzData?.message === 'Token tidak valid') {
+            toast.error('Sesi login kamu sudah habis. Silakan login ulang.');
+            setTimeout(() => {
+                signOut({ callbackUrl: '/admin/login' });
+            }, 5000);
+        }
+    }, [ustadzData]);
 
     return (
         <div>
